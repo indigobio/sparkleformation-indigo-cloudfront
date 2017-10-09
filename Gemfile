d332ee692817cb02775d2c4e6430cee54b1177c4
Greg Swallow <gswallow@gmail.com>
2017-10-09 14:24:50 -0400

    Add CNAME record to stabilize cloudfront.

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sfn'
 gem 'sparkle-pack-aws-my-s3-buckets',      :git => 'https://github.com/gswallow/sparkle-pack-aws-my-s3-buckets.git'
+gem 'sparkle-pack-aws-my-route53-zones',   :git => 'https://github.com/gswallow/sparkle-pack-aws-my-route53-zones.git'
 gem 'sparkle-pack-git-rev-outputs',        :git => 'https://github.com/gswallow/sparkle-pack-git-rev-outputs.git'
--- sparkleformation/cloudfront.rb
+++ sparkleformation/cloudfront.rb
@@ -1,7 +1,15 @@
 SparkleFormation.new(:cloudfront).load(:base, :git_rev_outputs).overrides do
   description <<EOF
-CloudFront distribution pointing to static.#{ENV['public_domain']}
+CloudFront distribution pointing to static.#{ENV['public_domain']}.  CNAME record for assets.#{ENV['public_domain']}.
 EOF
 
   dynamic!(:cloudfront_distribution, 'assets', :origin => "static.#{ENV['public_domain']}")
+
+  dynamic!(:record_set, 'assets',
+           :type => 'CNAME',
+           :record => 'assets',
+           :target => 'AssetsCloudFrontDistribution',
+           :domain_name => ENV['public_domain'],
+           :attr => 'DomainName',
+           :ttl => 60)
 end
